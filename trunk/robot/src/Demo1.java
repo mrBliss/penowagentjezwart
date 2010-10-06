26d6714fa2df8a5e1a35e96824d34a7a8b545648
dewinant@gmail.com <dewinant@gmail.com@c71dd4b7-9ca0-9c9d-3681-94ba5eea66ad>
2010-10-06 15:49:58 +0000

    steer vervangen door rotate -> robot draait ter plekke

--- trunk/robot/src/Demo1.java
+++ trunk/robot/src/Demo1.java
@@ -1,27 +1,27 @@
 import lejos.nxt.*;
-import lejos.robotics.navigation.Pilot;
 import lejos.robotics.navigation.TachoPilot;
 import lejos.util.ButtonCounter;
 
 public class Demo1 {
 
     public static final float WHEEL_DIAMETER = 56.0f;
-    public static final float TRACK_WIDTH = 110.0f;
+    public static final float TRACK_WIDTH = 112.0f;
 
-    Pilot pilot;
+    TachoPilot pilot;
 
     public void go() {
         while (true) {
             ButtonCounter bc = new ButtonCounter();
             bc.count("Hoeken / |zijde|?");
+            pilot.setMoveSpeed(720);
             int hoeken = bc.getLeftCount();
             int lengteZijde = 100 * bc.getRightCount();
 
             for (int i = 0; i < hoeken; i++) {
                 pilot.travel(lengteZijde, false);
-                pilot.steer(100.0f, (360 / hoeken));
+                pilot.rotate((360.0f / hoeken), false);
             }
-
+            
             System.out.println("" + pilot.getTravelDistance());
             Button.waitForPress();
         }
