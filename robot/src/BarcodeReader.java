1d7762da0f594fcc7634643fab03be20eae48b74
vervlietnico <vervlietnico@c71dd4b7-9ca0-9c9d-3681-94ba5eea66ad>
2010-10-11 16:58:37 +0000

    Barcode classes (initial import)

---
+++ robot/src/BarcodeReader.java
@@ -0,0 +1,18 @@
+
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class BarcodeReader implements Observer {
+
+	
+	private static ArrayList<Double> codeInMiliSeconds = new ArrayList<Double>();
+	
+	@Override	
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
